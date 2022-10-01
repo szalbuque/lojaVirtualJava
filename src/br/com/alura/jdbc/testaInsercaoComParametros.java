c37269d869d1169a48338411264c27b877d3119f
Silvia Rocha <szalbuque@gmail.com>
2022-10-01 20:32:43 -0300

    Usando try-with-resources

--- src/br/com/alura/jdbc/testaInsercaoComParametros.java
+++ src/br/com/alura/jdbc/testaInsercaoComParametros.java
@@ -13,24 +13,24 @@ public class testaInsercaoComParametros {
 	public static void main(String[] args) throws SQLException {
 		//Apesar de haver um erro na string abaixo, o prepareStatement vai contornar o erro antes de rodar o sql
 		ConnectionFactory factory = new ConnectionFactory();
-		Connection connection = factory.recuperarConexao();
-		//Assumimos a responsabilidade por fazer o commit das transações
-		connection.setAutoCommit(false);
-		
-		try {
-			PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)",Statement.RETURN_GENERATED_KEYS);
-			adicionaRegistro("Projetor", "Datashow", stm);
-			adicionaRegistro("Tela", "Tela de projeção", stm);
-			connection.commit();
-			stm.close();
-			connection.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("Rollback executado");
-			connection.rollback();
+		// try-with-resources
+		try (Connection connection = factory.recuperarConexao()){
+			//Assumimos a responsabilidade por fazer o commit das transações
+			connection.setAutoCommit(false);
+			// try-with-resources
+			try (PreparedStatement stm = 
+					connection.prepareStatement("INSERT INTO PRODUTO "
+					+ "(nome, descricao) VALUES (?,?)",
+					Statement.RETURN_GENERATED_KEYS);){
+				adicionaRegistro("Projetor", "Datashow", stm);
+				adicionaRegistro("Tela", "Tela de projeção", stm);
+				connection.commit();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("Rollback executado");
+				connection.rollback();
+			}
 		}
-		
-
 	}
 
 	private static void adicionaRegistro(String nome, String descricao, PreparedStatement stm) throws SQLException {
@@ -42,12 +42,13 @@ public class testaInsercaoComParametros {
 		}
 		stm.execute();
 
-		ResultSet rst = stm.getGeneratedKeys();
-		while(rst.next()) {
-			Integer id = rst.getInt(1);
-			System.out.println("O id criado foi: " + id);
+		// try-with-resources
+		try(ResultSet rst = stm.getGeneratedKeys()){
+			while(rst.next()) {
+				Integer id = rst.getInt(1);
+				System.out.println("O id criado foi: " + id);
 		}
-		rst.close();
+		}
 	}
 
 }
