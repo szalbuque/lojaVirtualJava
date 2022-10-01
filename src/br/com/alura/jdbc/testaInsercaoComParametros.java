30a855ba0baaa065a80e6f88a240dc9366343748
Silvia Rocha <szalbuque@gmail.com>
2022-10-01 17:56:53 -0300

    criada a classe de inserção com prepared statement

---
+++ src/br/com/alura/jdbc/testaInsercaoComParametros.java
@@ -0,0 +1,35 @@
+package br.com.alura.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+//vai usar o connection.prepareStatement ao invés do connection.createStatement, para evitar o SQL Injection
+
+public class testaInsercaoComParametros {
+
+	public static void main(String[] args) throws SQLException {
+		//Apesar de haver um erro na string abaixo, o prepareStatement vai contornar o erro antes de rodar o sql
+		String nome = "Fone'";
+		String descricao = "Fone sem microfone";
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection connection = factory.recuperarConexao();
+		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)",Statement.RETURN_GENERATED_KEYS);
+		stm.setString(1, nome);
+		stm.setString(2, descricao);
+		stm.execute();
+
+		ResultSet rst = stm.getGeneratedKeys();
+		while(rst.next()) {
+			Integer id = rst.getInt(1);
+			System.out.println("O id criado foi: " + id);
+		}
+		rst.close();
+		stm.close();
+		connection.close();
+
+	}
+
+}
